54bf3c03baffa7c23e5ad1fd646b702ccb3bd4d0
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2023-03-22 11:00:48 -0300

    REQ 3

---
+++ challenges/desafio3.sql
@@ -0,0 +1,9 @@
+SELECT 
+	u.nomeUsuario as pessoa_usuaria,
+    COUNT(c.idCancoes) as musicas_ouvidas,
+    ROUND(SUM(d.duracaoCancoes / 60), 2) as total_minutos
+    FROM Usuario u
+		JOIN Historico c ON u.idUsuario = c.idUsuario
+        JOIN Cancoes d ON d.idCancoes = c.idCancoes
+GROUP BY u.nomeUsuario
+ORDER BY nomeUsuario;       
